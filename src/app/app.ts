2b01532a8c053286ee509fb4cf8afd93f0269994
Rey Picazo <emamhdjej@gmail.com>
2026-08-27 22:22:38 -0600

    Pagina CV

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from "./pages/home/home";
+import { Navbar } from "./elementos/navbar/navbar";
+import { Footer } from "./elementos/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Home, Navbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/pages/home/home.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+  descargarCV(): void {
+    const link = document.createElement('a');
+    link.href = 'Rey_Emmanuel_Picazo_Perez_CV.pdf';
+    link.download = 'Rey_Emmanuel_Picazo_Perez_CV.pdf';
+    link.click();
+  }
+
+}
